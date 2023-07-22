4d81a4e5a47e4ab56c916595bf9c0fbf1d8e8737
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-07-23 02:37:30 +0330

    subfinder

--- s.py
+++ s.py
@@ -3,6 +3,11 @@ import argparse
 def print_string(domain):
     subdomains = set()
 
+    # Add common subdomains to check (e.g., www, mail, ftp, etc.)
+    common_subdomains = ["www", "mail", "ftp", "admin", "blog", "api"]
+    for subdomain in common_subdomains:
+        subdomains.add(f"{subdomain}.{domain}")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--print-string", type=str, help="Print the provided string.")
