3464c1945b8dc43ad40e0cf551a1a9ccc22746b3
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-07-23 08:26:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 import argparse
 import socket
+import requests
+import re
+
+def check_url_status(url):
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            return True
+        else:
+            return False            
+    except requests.Timeout:
+        return False
+    except requests.RequestException as e:
+        return False
+
+
+# def validate_url(url):
+#     # Regular expression pattern to match URLs
+#     url_pattern = re.compile(
+#         r'(?:[a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+'  # Domain (e.g., www.example.com)
+#         r'(?:\.[a-zA-Z]{2,})'  # Top-level domain (e.g., .com, .org)
+#         r'(?::\d+)?'  # Optional port number (e.g., :80)
+#         r'(?:/[^\s]*)?$'  # Optional path (e.g., /page or /path/to/page)
+#     )
+
+#     # Check if the URL matches the pattern
+#     return bool(url_pattern.match(url))
+
+
 
 def finder(domain):
     subdomains = set()
 
     # Add common subdomains to check (e.g., www, mail, ftp, etc.)
-    common_subdomains = ["www", "mail", "ftp", "admin", "blog", "api"]
+    common_subdomains = ["www", "mail", "ftp", "admin", "blog", "api", "accounts", "adwords"]
     for subdomain in common_subdomains:
         subdomains.add(f"{subdomain}.{domain}")
     
@@ -40,11 +69,12 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--print-string", type=str, help="Print the provided string.")
     args = parser.parse_args()
 
+
     if args.print_string:  # Corrected the attribute name here
         subdomains = finder(args.print_string)  # Corrected the attribute name here
         print(f"Subdomains of {args.print_string}:")
         for subdomain in subdomains:
-            print(subdomain)
-
+            status = check_url_status(url="http://"+subdomain)
+            print(f'{subdomain} status: {status}')
     else:
         print("Error: Please provide the string using the -d flag.")
