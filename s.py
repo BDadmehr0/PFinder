638af41872720ea469c9af4bfa3b760500481325
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-07-23 07:09:09 +0330

    Update s.py

--- s.py
+++ s.py
@@ -1,50 +1,68 @@
 import argparse
 import socket
+import requests
 
-def finder(domain):
-    subdomains = set()
-
-    # Add common subdomains to check (e.g., www, mail, ftp, etc.)
-    common_subdomains = ["www", "mail", "ftp", "admin", "blog", "api", "accounts", "adwords"]
-    for subdomain in common_subdomains:
-        subdomains.add(f"{subdomain}.{domain}")
-    
+def check_link_validity(link):
     try:
-        # Fetch DNS A records for the domain
-        for record_type in ['A', 'CNAME']:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect(('8.8.8.8', 53))  # Use Google's public DNS server
-                query = f"{domain} {record_type}\x00\x01"
-                s.sendall(query.encode())
+        response = requests.head(link, timeout=5)  # Add timeout of 5 seconds
+        if response.status_code >= 200 and response.status_code < 400:
+            return True
+        else:
+            return False
+    except requests.exceptions.RequestException:
+        return False
 
-                data = s.recv(4096)
-                while data:
-                    # Parse DNS response to extract subdomains
-                    index = data.find(query.encode())
-                    if index == -1:
-                        break
+# Example usage
+link_to_check = "www.example.com"
+if check_link_validity(link_to_check):
+    print("Link is valid.")
+else:
+    print("Link is invalid.")
 
-                    start = index + len(query) + 1
-                    length = data[start]
-                    subdomain = data[start + 1: start + 1 + length].decode()
-                    subdomains.add(subdomain)
+# def finder(domain):
+#     subdomains = set()
 
-                    data = data[start + 1 + length:]
-    except (socket.gaierror, ConnectionRefusedError, OSError):
-        pass
+#     # Add common subdomains to check (e.g., www, mail, ftp, etc.)
+#     common_subdomains = ["www", "mail", "ftp", "admin", "blog", "api", "accounts", "adwords"]
+#     for subdomain in common_subdomains:
+#         subdomains.add(f"{subdomain}.{domain}")
+    
+#     try:
+#         # Fetch DNS A records for the domain
+#         for record_type in ['A', 'CNAME']:
+#             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#                 s.connect(('8.8.8.8', 53))  # Use Google's public DNS server
+#                 query = f"{domain} {record_type}\x00\x01"
+#                 s.sendall(query.encode())
 
-    return subdomains
+#                 data = s.recv(4096)
+#                 while data:
+#                     # Parse DNS response to extract subdomains
+#                     index = data.find(query.encode())
+#                     if index == -1:
+#                         break
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--print-string", type=str, help="Print the provided string.")
-    args = parser.parse_args()
+#                     start = index + len(query) + 1
+#                     length = data[start]
+#                     subdomain = data[start + 1: start + 1 + length].decode()
+#                     subdomains.add(subdomain)
 
-    if args.print_string:  # Corrected the attribute name here
-        subdomains = finder(args.print_string)  # Corrected the attribute name here
-        print(f"Subdomains of {args.print_string}:")
-        for subdomain in subdomains:
-            print(subdomain)
+#                     data = data[start + 1 + length:]
+#     except (socket.gaierror, ConnectionRefusedError, OSError):
+#         pass
 
-    else:
-        print("Error: Please provide the string using the -d flag.")
+#     return subdomains
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-d", "--print-string", type=str, help="Print the provided string.")
+#     args = parser.parse_args()
+
+#     if args.print_string:  # Corrected the attribute name here
+#         subdomains = finder(args.print_string)  # Corrected the attribute name here
+#         print(f"Subdomains of {args.print_string}:")
+#         for subdomain in subdomains:
+#             print(subdomain)
+
+#     else:
+#         print("Error: Please provide the string using the -d flag.")
