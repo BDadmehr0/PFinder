4994e86c2f0d705ff03f020cc0cc9c2989812505
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-07-23 07:22:36 +0330

    Update s.py

--- s.py
+++ s.py
@@ -2,67 +2,71 @@ import argparse
 import socket
 import requests
 
-def check_link_validity(link):
-    try:
-        response = requests.head(link, timeout=5)  # Add timeout of 5 seconds
-        if response.status_code >= 200 and response.status_code < 400:
-            return True
-        else:
-            return False
-    except requests.exceptions.RequestException:
-        return False
+import re
 
-# Example usage
-link_to_check = "www.example.com"
-if check_link_validity(link_to_check):
-    print("Link is valid.")
-else:
-    print("Link is invalid.")
+def validate_url(url):
+    # Regular expression pattern to match URLs
+    url_pattern = re.compile(
+        r'^(?:http|https)://'  # Scheme (http or https)
+        r'(?:[a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+'  # Domain (e.g., www.example.com)
+        r'(?:\.[a-zA-Z]{2,})'  # Top-level domain (e.g., .com, .org)
+        r'(?::\d+)?'  # Optional port number (e.g., :80)
+        r'(?:/[^\s]*)?$'  # Optional path (e.g., /page or /path/to/page)
+    )
 
-# def finder(domain):
-#     subdomains = set()
+    # Check if the URL matches the pattern
+    return bool(url_pattern.match(url))
 
-#     # Add common subdomains to check (e.g., www, mail, ftp, etc.)
-#     common_subdomains = ["www", "mail", "ftp", "admin", "blog", "api", "accounts", "adwords"]
-#     for subdomain in common_subdomains:
-#         subdomains.add(f"{subdomain}.{domain}")
+
+
+def finder(domain):
+    subdomains = set()
+
+    # Add common subdomains to check (e.g., www, mail, ftp, etc.)
+    common_subdomains = ["www", "mail", "ftp", "admin", "blog", "api", "accounts", "adwords"]
+    for subdomain in common_subdomains:
+        subdomains.add(f"{subdomain}.{domain}")
     
-#     try:
-#         # Fetch DNS A records for the domain
-#         for record_type in ['A', 'CNAME']:
-#             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#                 s.connect(('8.8.8.8', 53))  # Use Google's public DNS server
-#                 query = f"{domain} {record_type}\x00\x01"
-#                 s.sendall(query.encode())
+    try:
+        # Fetch DNS A records for the domain
+        for record_type in ['A', 'CNAME']:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect(('8.8.8.8', 53))  # Use Google's public DNS server
+                query = f"{domain} {record_type}\x00\x01"
+                s.sendall(query.encode())
 
-#                 data = s.recv(4096)
-#                 while data:
-#                     # Parse DNS response to extract subdomains
-#                     index = data.find(query.encode())
-#                     if index == -1:
-#                         break
+                data = s.recv(4096)
+                while data:
+                    # Parse DNS response to extract subdomains
+                    index = data.find(query.encode())
+                    if index == -1:
+                        break
 
-#                     start = index + len(query) + 1
-#                     length = data[start]
-#                     subdomain = data[start + 1: start + 1 + length].decode()
-#                     subdomains.add(subdomain)
+                    start = index + len(query) + 1
+                    length = data[start]
+                    subdomain = data[start + 1: start + 1 + length].decode()
+                    subdomains.add(subdomain)
 
-#                     data = data[start + 1 + length:]
-#     except (socket.gaierror, ConnectionRefusedError, OSError):
-#         pass
+                    data = data[start + 1 + length:]
+    except (socket.gaierror, ConnectionRefusedError, OSError):
+        pass
 
-#     return subdomains
+    return subdomains
 
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("-d", "--print-string", type=str, help="Print the provided string.")
-#     args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--print-string", type=str, help="Print the provided string.")
+    args = parser.parse_args()
 
-#     if args.print_string:  # Corrected the attribute name here
-#         subdomains = finder(args.print_string)  # Corrected the attribute name here
-#         print(f"Subdomains of {args.print_string}:")
-#         for subdomain in subdomains:
-#             print(subdomain)
 
-#     else:
-#         print("Error: Please provide the string using the -d flag.")
+    if args.print_string:  # Corrected the attribute name here
+        if validate_url(args.print_string):
+            subdomains = finder(args.print_string)  # Corrected the attribute name here
+            print(f"Subdomains of {args.print_string}:")
+            for subdomain in subdomains:
+                print(subdomain)
+        else:
+            print()
+
+    else:
+        print("Error: Please provide the string using the -d flag.")
