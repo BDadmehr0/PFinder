d7129a841f3af155a1a174ad031d54d706a02537
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-09-10 05:34:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def finder(domain):
     # Add common subdomains to check (e.g., www, mail, ftp, etc.)
     common_subdomains = ["www", "mail", "ftp", "admin", "blog", "api",
                          "accounts", "adwords", "emails", "pkg", "smtp",
-                         "support", ]
+                         "support", "api"]
     for subdomain in common_subdomains:
         subdomains.add(f"{subdomain}.{domain}")
     
